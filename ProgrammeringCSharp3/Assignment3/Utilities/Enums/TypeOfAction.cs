e16fd26f02d285a26ed4cd02b70994e43f3cf91e
oberntorp <oberntorp@hotmail.com>
2020-11-23 12:49:53 +0100

    - Utilities referenced wrong version - Assignment 3

---
+++ ProgrammeringCSharp3/Assignment3/Utilities/Enums/TypeOfAction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities.Enums
+{
+    /// <summary>
+    /// This enum is used in conjunction with the user control used for Adding/Editing
+    /// </summary>
+    public enum Mode
+    {
+        Edit,
+        Add
+    }
+}
