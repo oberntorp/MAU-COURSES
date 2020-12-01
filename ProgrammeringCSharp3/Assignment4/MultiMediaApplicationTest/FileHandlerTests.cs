45826c499ce4a5b2881a385fac59326639e4a7cd
oberntorp <oberntorp@hotmail.com>
2020-12-01 18:30:00 +0100

    - Tests for FileHandler added

---
+++ ProgrammeringCSharp3/Assignment4/MultiMediaApplicationTest/FileHandlerTests.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Utilities;
+
+namespace MultiMediaApplicationTest
+{
+    [TestClass]
+    public class FileHandlerTests
+    {
+        [TestMethod]
+        public void GetFileName_CurrectFileNameTest()
+        {
+            // Arrange
+            string fileName = "C:\\Users\\obern\\OneDrive\\Bilder\\DSC08984.JPG";
+            string expected = "DSC08984";
+
+            // Act
+            string actual = FileHandler.GetFileName(fileName);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetFileName_WithOutFilePathTest()
+        {
+            // Arrange
+            string fileName = string.Empty;
+            // Act
+            FileHandler.GetFileName(fileName);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetFileName_WithErrorNoDotInFileNameTest()
+        {
+            // Arrange
+            string fileName = "file";
+            // Act
+            FileHandler.GetFileName(fileName);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetFileName_WithErrorIncompleteFileNameTest()
+        {
+            // Arrange
+            string fileName = "file.";
+            // Act
+            FileHandler.GetFileName(fileName);
+        }
+
+        public void GetFileExtention_CurrectFileNameTest()
+        {
+            // Arrange
+            string fileName = "C:\\Users\\obern\\OneDrive\\Bilder\\DSC08984.JPG";
+            string expected = "JPG";
+
+            // Act
+            string actual = FileHandler.GetFileName(fileName);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetFileExtention_WithOutFilePathTest()
+        {
+            // Arrange
+            string fileName = string.Empty;
+            // Act
+            FileHandler.GetFileName(fileName);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetFileExtention_WithErrorNoDotInFileNameTest()
+        {
+            // Arrange
+            string fileName = "file";
+            // Act
+            FileHandler.GetFileName(fileName);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetFileExtention_WithErrorIncompleteFileNameTest()
+        {
+            // Arrange
+            string fileName = "file.";
+            // Act
+            FileHandler.GetFileName(fileName);
+        }
+    }
+}
--- ProgrammeringCSharp3/Assignment4/Utilities/FileHandler.cs
+++ ProgrammeringCSharp3/Assignment4/Utilities/FileHandler.cs
@@ -17,18 +17,49 @@ namespace Utilities
         /// <returns></returns>
         public static string GetFileName(string filePath)
         {
+            CheckforErrors(filePath.Split('\\').Last());
             return filePath.Split('\\').Last().Split('.').First();
         }
 
         /// <summary>
         /// Get a files file extention, througth the files file name
         /// </summary>
-        /// <param name="fileName">The file name</param>
+        /// <param name="filePath">The file name</param>
         /// <returns>the file extention as a string</returns>
         public static string GetFileExtension(string filePath)
         {
-
+            CheckforErrors(filePath.Split('\\').Last());
             return filePath.Split('\\').Last().Split('.').Last();
+        }
+
+        /// <summary>
+        /// Check for errors on file name, if found, throw errors
+        /// </summary>
+        /// <param name="filePath">The filePath to check for errors</param>
+        private static void CheckforErrors(string filePath)
+        {
+            if (filePath == string.Empty)
+            {
+                throw new ArgumentException("No file path specified, can´t retreive file name");
+            }
+            if (!filePath.Contains("."))
+            {
+                throw new ArgumentException("Please check the file name, it is not possible to retreive the file name");
+            }
+            if (filePath.Contains(".") && !IsFileNameComplete(filePath))
+            {
+                throw new ArgumentException("Please check the file name, it is not possible to retreive the file name, either no name or file extention");
+            }
+        }
+
+        /// <summary>
+        /// Checks that the filePath contains file name and file extention
+        /// </summary>
+        /// <param name="filePath">The filePath to check for errors</param>
+        /// <returns></returns>
+        private static bool IsFileNameComplete(string filePath)
+        {
+            return filePath.Split('.')[0] != string.Empty && filePath.Split('.')[1] != string.Empty;
         }
     }
 }
