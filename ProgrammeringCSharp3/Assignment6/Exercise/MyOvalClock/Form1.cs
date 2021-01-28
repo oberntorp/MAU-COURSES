cdd6bc12e017074f17ffa5b737c9586074279320
oberntorp <oberntorp@hotmail.com>
2021-01-28 18:07:50 +0100

    Exercise on graphics C#

---
+++ ProgrammeringCSharp3/Assignment6/Exercise/MyOvalClock/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyOvalClock
+{
+    public partial class Form1 : Form
+    {
+        private string bg = Path.Combine(Application.StartupPath, "9F11BD6A-8BFC-4E04-A706-E4E485B07E13.jpg");
+        public Form1()
+        {
+            InitializeComponent();
+            FormBorderStyle = FormBorderStyle.None;
+            DoubleBuffered = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+
+            MyOvalClock clockObj = new MyOvalClock();
+
+            if(!File.Exists(bg))
+            {
+                timer1.Stop();
+                MessageBox.Show("Please copy an background to the bin debug folder");
+            }
+            if(!clockObj.DrawGraphicPathClipDrawingArea(e.Graphics, ClientRectangle, bg))
+            {
+                Application.Exit();
+            }
+
+            clockObj.DrawSystemTime(e.Graphics, ClientRectangle);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Rectangle rect = new Rectangle(0, 0, Width, Height);
+            Invalidate(rect);
+        }
+    }
+}
---
+++ ProgrammeringCSharp3/Assignment6/Exercise/MyOvalClock/MyOvalClock.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyOvalClock
+{
+    public class MyOvalClock
+    {
+        public bool DrawGraphicPathClipDrawingArea(Graphics graphics, Rectangle clientAreaOfForm, string pictureToUseBg)
+        {
+            int rectangleX = clientAreaOfForm.Width;
+            int rectangleY = clientAreaOfForm.Height;
+
+            GraphicsPath graphicsPath = new GraphicsPath();
+
+            Rectangle boundaryOfForm = new Rectangle(0, 0, rectangleX, rectangleY);
+            graphicsPath.AddPie(boundaryOfForm, 40, 370);
+            graphics.SetClip(graphicsPath);
+
+            Pen pen = new Pen(Color.Blue, 10);
+
+            try
+            {
+                graphics.DrawImage(Image.FromFile(pictureToUseBg), boundaryOfForm);
+            }
+            catch
+            {
+                return false;
+            }
+
+            graphics.DrawPath(pen, graphicsPath);
+
+            return true;
+        }
+
+        public void DrawSystemTime(Graphics graphics, Rectangle clientAreaOfForm)
+        {
+            int fontSize = 70;
+
+            Font fontUsed = new Font("Times New Roman", fontSize, FontStyle.Regular);
+
+            SolidBrush brush = new SolidBrush(Color.Green);
+
+            string stringToDraw = DateTime.Now.ToLongTimeString();
+
+            SizeF stringSizeBeingDrawn = graphics.MeasureString(stringToDraw, fontUsed);
+
+            float xAxisPos = clientAreaOfForm.Width / 2.0F - stringSizeBeingDrawn.Width / 2.0F;
+            float yAxisPos = clientAreaOfForm.Height / 3.5F - fontSize;
+
+
+            graphics.DrawString(stringToDraw, fontUsed, brush, xAxisPos, yAxisPos);
+
+            fontUsed.Dispose();
+            brush.Dispose();
+        }
+    }
+}
