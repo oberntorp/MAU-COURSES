bf3a622033f8111d9badf6e51c9f53474d3a799b
oberntorp <oberntorp@hotmail.com>
2021-01-29 18:48:14 +0100

    Changes made:
    Begun construction of diagram header and axis.

---
+++ ProgrammeringCSharp3/Assignment6/Assignment6/Diagram/DiagramGenerator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Assignment6.Diagram
+{
+    class DiagramGenerator: Canvas
+    {
+        private string diagramTitle;
+        private int diagramMin;
+        private int diagramMax;
+        private int diagramStep;
+
+        public DiagramGenerator(double width, double  height, string diagramTitleFromGui, int diagramIntervalMinFromGui, int diagramIntervalMaxFromGui, int diagramStepFromGui)
+        {
+            Width = width;
+            Height = height;
+            diagramTitle = diagramTitleFromGui;
+            diagramMin = diagramIntervalMinFromGui;
+            diagramMax = diagramIntervalMaxFromGui;
+            diagramStep = diagramStepFromGui;
+        }
+
+        protected override void OnRender(DrawingContext dc)
+        {
+            base.OnRender(dc);
+
+            FlowDirection dir = FlowDirection.LeftToRight;
+            FormattedText textToRender = new FormattedText(diagramTitle, new System.Globalization.CultureInfo("se-SV"), dir, new Typeface("Arial"), 102, Brushes.Blue, VisualTreeHelper.GetDpi(this).PixelsPerDip);
+
+            dc.DrawText(textToRender, new Point(50, 0));
+
+            Pen yAxis = new Pen(Brushes.Blue, 4);
+            yAxis.StartLineCap = PenLineCap.Triangle;
+
+            Pen xAxis = new Pen(Brushes.Black, 4);
+            xAxis.EndLineCap = PenLineCap.Triangle;
+
+            dc.DrawLine(yAxis, new Point(50, 105), new Point(50, 280));
+            dc.DrawLine(xAxis, new Point(50, 280), new Point(280, 280));
+        }
+    }
+}
--- ProgrammeringCSharp3/Assignment6/Assignment6/MainWindow.xaml.cs
+++ ProgrammeringCSharp3/Assignment6/Assignment6/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Assignment6.Diagram;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,30 @@ namespace Assignment6
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void SaveSettingsButton_Click(object sender, RoutedEventArgs e)
+        {
+            string diagramTitle = DiaTitleTextBox.Text;
+            string diagramMin = DiaIntervalMinTextBox.Text;
+            string diagramMax = DiaIntervalMaxTextBox.Text;
+            string diagramStep = DiaIntervallStepTextBox.Text;
+
+
+            if (AllDataEntered(diagramTitle, diagramMin, diagramMax, diagramStep))
+            {
+                DiagramGenerator dGenerator = new DiagramGenerator(Width, Height, diagramTitle, int.Parse(diagramMin), int.Parse(diagramMax), int.Parse(diagramStep));
+                DiagramGrid.Children.Add(dGenerator);
+            }
+            else
+            {
+                MessageBox.Show("You have not entered all data needed", "InvalidData", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private bool AllDataEntered(string diagramTitle, string diagramMin, string diagramMax, string diagramStep)
+        {
+            return !(string.IsNullOrEmpty(diagramTitle) || string.IsNullOrEmpty(diagramMin) || string.IsNullOrEmpty(diagramMax) || string.IsNullOrEmpty(diagramStep));
         }
     }
 }
