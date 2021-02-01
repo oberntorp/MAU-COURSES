e956223de386f7e5b78d95df4d871cd7555286c3
oberntorp <oberntorp@hotmail.com>
2021-02-01 19:24:02 +0100

    Changes Made: - Numbers now present in diagram - Appearence of tickmarks - Y axis  print order changed - Refactorings

--- ProgrammeringCSharp3/Assignment6/Assignment6/Diagram/DiagramGenerator.cs
+++ ProgrammeringCSharp3/Assignment6/Assignment6/Diagram/DiagramGenerator.cs
@@ -61,37 +61,55 @@ namespace Assignment6.Diagram
             yAxisPen.StartLineCap = PenLineCap.Triangle;
             dc.DrawLine(yAxisPen, new Point(50, 105), new Point(50, maxLengthYAxis));
 
-            DrawIntervalsYAxis(dc, yAxisPen);
+            DrawIntervalsYAxis(dc);
         }
 
-        private void DrawIntervalsYAxis(DrawingContext dc, Pen yAxisPen)
+        private void DrawIntervalsYAxis(DrawingContext dc)
         {
-            yAxisPen.Thickness = 1;
+            Pen yAxisPen = new Pen(Brushes.Black, 2);
             int interval = sixeOfAxes / divisionsY;
-            for (int i = (divisionsY/intervalY)+105, intervalFigure = intervalY; i < maxLengthYAxis-intervalY; i += interval, intervalFigure += intervalY)
+            for (int offsetYAxis = maxLengthYAxis - interval, intervalFigure = intervalY; offsetYAxis >= 105; offsetYAxis -= interval, intervalFigure += intervalY)
             {
-                dc.DrawLine(yAxisPen, new Point(45, i), new Point(55, i));
+                dc.DrawLine(yAxisPen, new Point(45, offsetYAxis), new Point(55, offsetYAxis));
+                DrawYAxisNumbers(dc, intervalFigure.ToString(), offsetYAxis);
             }
+        }
+
+        private void DrawYAxisNumbers(DrawingContext dc, string intToDraw, int positionOfInt)
+        {
+            FlowDirection dir = FlowDirection.LeftToRight;
+            FormattedText textToRender = new FormattedText(intToDraw, new System.Globalization.CultureInfo("se-SV"), dir, new Typeface("Arial"), 10, Brushes.Green, VisualTreeHelper.GetDpi(this).PixelsPerDip);
+
+            dc.DrawText(textToRender, new Point(25, positionOfInt - 4));
         }
 
         private void DrawXAxis(DrawingContext dc)
         {
-            Pen xAxisPen = new Pen(Brushes.Black, 4);
-            xAxisPen.EndLineCap = PenLineCap.Triangle;
+            Pen xAxisTickPen = new Pen(Brushes.Black, 4);
+            xAxisTickPen.EndLineCap = PenLineCap.Triangle;
 
-            dc.DrawLine(xAxisPen, new Point(50, 105 + sixeOfAxes), new Point(maxLengthOfXAxis, maxLengthYAxis));
+            dc.DrawLine(xAxisTickPen, new Point(50, 105 + sixeOfAxes), new Point(maxLengthOfXAxis, maxLengthYAxis));
 
-            DrawIntervalsXAxis(dc, xAxisPen);
+            DrawIntervalsXAxis(dc);
         }
 
-        private void DrawIntervalsXAxis(DrawingContext dc, Pen xAxisPen)
+        private void DrawIntervalsXAxis(DrawingContext dc)
         {
-            xAxisPen.Thickness = 1;
+            Pen xAxisTickPen = new Pen(Brushes.Gray, 2);
             int interval = sixeOfAxes / divisionsX;
-            for (int offsetXAxis = (divisionsX/intervalX)+50, intervalFigure = intervalX; offsetXAxis < maxLengthOfXAxis-divisionsX; offsetXAxis += interval, intervalFigure += intervalX)
+            for (int offsetXAxis = interval+50, intervalFigure = intervalX; offsetXAxis <= maxLengthOfXAxis; offsetXAxis += interval, intervalFigure += intervalX)
             {
-                dc.DrawLine(xAxisPen, new Point(offsetXAxis, (105 + sixeOfAxes) - 5), new Point(offsetXAxis, (105 + sixeOfAxes) + 5));
+                dc.DrawLine(xAxisTickPen, new Point(offsetXAxis, (105 + sixeOfAxes) - 5), new Point(offsetXAxis, (105 + sixeOfAxes) + 5));
+                DrawXAxisNumbers(dc, intervalFigure.ToString(), offsetXAxis);
             }
+        }
+
+        private void DrawXAxisNumbers(DrawingContext dc, string intToDraw, int positionOfInt)
+        {
+            FlowDirection dir = FlowDirection.LeftToRight;
+            FormattedText textToRender = new FormattedText(intToDraw, new System.Globalization.CultureInfo("se-SV"), dir, new Typeface("Arial"), 10, Brushes.Green, VisualTreeHelper.GetDpi(this).PixelsPerDip);
+
+            dc.DrawText(textToRender, new Point(positionOfInt - 4, (105 + sixeOfAxes) + 15));
         }
     }
 }
